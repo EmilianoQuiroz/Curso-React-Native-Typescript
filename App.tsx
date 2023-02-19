52e51fb00219489932df5acf25505ff8c0c2c96f
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2023-02-19 03:18:57 -0300

    Dimenciones

--- App.tsx
+++ App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 //import { ContadorScreen } from './src/screens/ContadorScreen';
+//import { BoxObjetModelScreen } from './src/screens/BoxObjetModelScreen';//Importamos la Screen de BoxObjetModelScreen
 import { SafeAreaView } from 'react-native';//Con SafeAreaView verificamos si el componente se muestra en pantalla
-import { BoxObjetModelScreen } from './src/screens/BoxObjetModelScreen';//Importamos la Screen de BoxObjetModelScreen
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 
 export const App = () => {
   return (
     //<ContadorScreen />
-    <SafeAreaView style={{
-      flex: 1
-    }}>
-      
-      <BoxObjetModelScreen />
-    
-    </SafeAreaView>
+    // <SafeAreaView style={{
+    //   flex: 1
+    // }}>
+      <DimensionesScreen />
+    // </SafeAreaView>
     )
 }
 
